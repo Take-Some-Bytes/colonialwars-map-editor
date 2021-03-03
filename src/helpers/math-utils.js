7e4f60545d023abd6467299d09e5609dbeab6fac
Take-Some-Bytes <horton0712@gmail.com>
2021-03-03 14:41:04 -0800

    Added method to find all divisors of a number

--- src/helpers/math-utils.js
+++ src/helpers/math-utils.js
@@ -27,3 +27,29 @@ export function inBound (val, min, max) {
   if (min > max) { return val >= max && val <= min }
   return val >= min && val <= max
 }
+/**
+ * Finds all the divisors (i.e. factors) of num.
+ * @param {number} num The number to find all the divisors of.
+ * @returns {Array<number>}
+ */
+export function findAllDivisors (num) {
+  let divisors = []
+
+  for (let i = 0, stop = Math.sqrt(num); i < stop; i++) {
+    if (num % i === 0) {
+      if (i === num / i) {
+        divisors.push(i)
+      } else {
+        divisors.push(i, num / i)
+      }
+    }
+  }
+
+  // Sort the divisors from least to greatest.
+  divisors = divisors
+    // All values sorted must be a number, not NaN and not Infinity.
+    .filter(val => typeof val === 'number' && !isNaN(val) && isFinite(val))
+    .sort((a, b) => a - b)
+
+  return divisors
+}

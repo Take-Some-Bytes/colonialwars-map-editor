0c1513e3e6946a0ed2c51ee5be6486d599d10cbd
Take-Some-Bytes <horton0712@gmail.com>
2021-05-26 21:17:40 -0700

    Updated app constants

--- src/constants.js
+++ src/constants.js
@@ -4,17 +4,35 @@
  */
 
 /**
+  * Deep-freezes an object.
+  * @param {O} object The object to deep freeze.
+  * @returns {Readonly<O>}
+  * @template O
+  */
+function deepFreeze (object) {
+  // Retrieve the property names defined on object.
+  const propNames = Object.getOwnPropertyNames(object)
+  // Freeze properties before freezing self.
+  for (const name of propNames) {
+    const value = object[name]
+    if (value && typeof value === 'object') {
+      deepFreeze(value)
+    }
+  }
+  return Object.freeze(object)
+}
+
+/**
  * Client constants.
  */
-export default Object.freeze({
+export default deepFreeze({
   FALLBACKS: {
     STARTING_MAP_CONFIG: {
-      size: {
-        x: 60,
-        y: 60
-      },
+      mode: 'teams',
+      defaultHeight: 0,
       tileType: 'grass',
-      defaultHeight: 0
+      size: { x: 60, y: 60 },
+      dataFiles: { unit: 'none', building: 'none', graphics: 'none' }
     }
   },
   GAME_CONSTANTS: {
@@ -22,6 +40,13 @@ export default Object.freeze({
     VIEWPORT_STICKINESS: 0.004,
     DRAWING_TILE_SIZE: 100,
     TILES_META_FILE: '/meta/tiles.meta.json'
+  },
+  DRAWING_CONSTANTS: {
+    baseUrl: '/imgs/game-images/',
+    tileFrames: {
+      grass: [0, 0],
+      sand: [1, 0]
+    }
   },
   IMAGE_DRAWER_CONFIG: {
     imgDir: '/imgs/game-images',
@@ -34,5 +59,7 @@ export default Object.freeze({
     MAX_MAP_SIZE: 200,
     MIN_DEFAULT_HEIGHT: 0,
     MAX_DEFAULT_HEIGHT: 2
-  }
+  },
+  VALID_TILE_TYPES: ['grass', 'sand', 'rock'],
+  VALID_GAME_MODES: ['teams', 'koth', 'siege']
 })

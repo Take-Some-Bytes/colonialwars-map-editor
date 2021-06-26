a422ce7b3dc51a112c58a3374cd13b8cf8a99f34
Take-Some-Bytes <horton0712@gmail.com>
2021-06-26 15:22:27 -0700

    Add editor modals, update editor state tracking
    - The different editor states are now collected into an "enum" in
    the constants file.

--- src/layout/editor-container.jsx
+++ src/layout/editor-container.jsx
@@ -4,34 +4,31 @@
  */
 
 import React from 'react'
+import debugFactory from 'debug'
 
+import Loading from '../components/loading.jsx'
 import EditorCanvas from './editor/canvas.jsx'
 import EditorInterface from './editor/interface.jsx'
+import { EditorModals } from './modals/index.jsx'
 
 import Editor from '../editor/editor.js'
-import Loading from '../components/loading.jsx'
 
-import debugFactory from 'debug'
+import constants from '../constants.js'
 
 import { saveAs } from 'file-saver'
 
 const debug = debugFactory('cw-map-editor:editor-container')
 
-export const EDITOR_STATE_RUNNING = 0
-export const EDITOR_STATE_STARTING = 1
-export const EDITOR_STATE_SUSPENDED = 2
-export const EDITOR_STATE_HAD_ERROR = 3
-export const EDITOR_STATE_NOT_STARTED = 4
-export const EDITOR_STATE_DO_NOT_START = 5
-
 /**
  * @typedef {Object} EditorContainerProps
  * @prop {boolean} show
  * @prop {() => void} closeEditor
+ * @prop {() => void} openMapTeamsModal
+ * @prop {React.Dispatch<any>} setError
  * @prop {() => Promise<boolean>} openNewMapModal
  * @prop {Record<string, any>} mapConfig
  * @prop {Record<string, string>} keyBindings
- * @prop {import('../components/custom-modal').Dimensions} viewportDimensions
+ * @prop {import('../components/custom-modal').Dimensions} vwDimensions
  */
 
 /**
@@ -42,135 +39,174 @@ export const EDITOR_STATE_DO_NOT_START = 5
 export default function EditorContainer (props) {
   const [editor, setEditor] = React.useState(null)
   const [editorLoading, setEditorLoading] = React.useState(false)
-  const editorState = React.useRef(EDITOR_STATE_DO_NOT_START)
-  // const [editorState, setEditorState] = React.useState(EDITOR_STATE_NOT_STARTED)
+  const [teamsModalOpen, setTeamsModalOpen] = React.useState(false)
+  const [settingsModalOpen, setSettingsModalOpen] = React.useState(false)
+  const forceUpdate = React.useReducer(() => ({}))[1]
+  const editorState = React.useRef(constants.EDITOR_STATE.DO_NOT_START)
 
   // If we're going to show the editor, set editor state to EDITOR_STATE_NOT_STARTED.
-  if (props.show && editorState.current === EDITOR_STATE_DO_NOT_START) {
-    editorState.current = EDITOR_STATE_NOT_STARTED
+  if (props.show && editorState.current === constants.EDITOR_STATE.DO_NOT_START) {
+    editorState.current = constants.EDITOR_STATE.NOT_STARTED
   }
 
   /**
    * @type {import('../helpers/hooks').DrawFunction<'2d'>}
    */
   function draw (ctx) {
-    // debugger
     if (
-      editorState.current === EDITOR_STATE_DO_NOT_START ||
-      editorState.current === EDITOR_STATE_HAD_ERROR ||
-      editorState.current === EDITOR_STATE_STARTING
+      editorState.current === constants.EDITOR_STATE.DO_NOT_START ||
+      editorState.current === constants.EDITOR_STATE.HAD_ERROR ||
+      editorState.current === constants.EDITOR_STATE.STARTING
     ) {
-    // if (editorState === EDITOR_STATE_HAD_ERROR || editorState === EDITOR_STATE_STARTING) {
       // Do nothing.
       // The editor either had an error, is already starting, or is not shown.
       return
     }
 
-    if (editorState.current === EDITOR_STATE_RUNNING && editor instanceof Editor) {
-    // if (editorState === EDITOR_STATE_RUNNING && editor instanceof Editor) {
+    if (editorState.current === constants.EDITOR_STATE.RUNNING && editor instanceof Editor) {
       // Editor has started. Run it.
       try {
         editor.run()
       } catch (ex) {
         console.error(ex.stack)
-        editorState.current = EDITOR_STATE_HAD_ERROR
-        // setEditorState(EDITOR_STATE_HAD_ERROR)
+        editorState.current = constants.EDITOR_STATE.HAD_ERROR
       }
       return
     }
 
-    if (editorState.current === EDITOR_STATE_NOT_STARTED) {
-    // if (editorState === EDITOR_STATE_NOT_STARTED) {
-      // editorStarting.current = true
-      // setEditorState(EDITOR_STATE_STARTING)
+    if (editorState.current === constants.EDITOR_STATE.NOT_STARTED) {
       debug('Starting editor')
-      editorState.current = EDITOR_STATE_STARTING
+      editorState.current = constants.EDITOR_STATE.STARTING
       setEditorLoading(true)
-      Editor.create(props.mapConfig, props.keyBindings, ctx, props.viewportDimensions)
+      Editor.create(props.mapConfig, props.keyBindings, ctx, props.vwDimensions)
         .then(edtr => {
-          // editorStarting.current = false
           setEditor(edtr)
-          // setEditorState(EDITOR_STATE_RUNNING)
-          editorState.current = EDITOR_STATE_RUNNING
+          editorState.current = constants.EDITOR_STATE.RUNNING
           setEditorLoading(false)
         })
         .catch(ex => {
           console.error(ex.stack)
-          // setEditorState(EDITOR_STATE_HAD_ERROR)
-          editorState.current = EDITOR_STATE_HAD_ERROR
+          editorState.current = constants.EDITOR_STATE.HAD_ERROR
         })
     }
   }
+  /**
+   * Convenience function to suspend the editor.
+   */
+  function suspendEditor () {
+    debug('Suspending editor.')
+    editor.suspend()
+    editorState.current = constants.EDITOR_STATE.SUSPENDED
+  }
+  /**
+   * Convenience function to suspend the editor.
+   */
+  function unsuspendEditor () {
+    debug('Unsuspending editor.')
+    editor.unsuspend()
+    editorState.current = constants.EDITOR_STATE.RUNNING
+  }
 
   return (
-    <div className='editor-container' style={{ display: props.show ? 'block' : 'none' }}>
-      <EditorCanvas
-        dimensions={{
-          width: props.viewportDimensions.width,
-          height: props.viewportDimensions.height
-        }}
-        draw={draw}
-      />
-      <EditorInterface
-        quit={() => {
-          if (editor instanceof Editor) {
-            // Do this so that we stop taking input.
-            editor.stop()
-          }
+    <>
+      <div className='editor-container' style={{ display: props.show ? 'block' : 'none' }}>
+        <EditorCanvas
+          dimensions={{
+            width: props.vwDimensions.width,
+            height: props.vwDimensions.height
+          }}
+          draw={draw}
+        />
+        <EditorInterface
+          quit={() => {
+            if (editor instanceof Editor) {
+              // Do this so that we stop taking input.
+              editor.stop()
+            }
 
-          editorState.current = EDITOR_STATE_DO_NOT_START
-          setEditor(null)
-          props.closeEditor()
-        }}
-        openNewMapModal={() => {
-          if (editor instanceof Editor) {
-            // Suspend the editor while the user messes with the new map modal.
-            debug('Suspending editor.')
-            editor.suspend()
-            editorState.current = EDITOR_STATE_SUSPENDED
-          }
+            editorState.current = constants.EDITOR_STATE.DO_NOT_START
+            setEditor(null)
+            props.closeEditor()
+          }}
+          openNewMapModal={() => {
+            if (editor instanceof Editor) {
+              // Suspend the editor while the user messes with the new map modal.
+              suspendEditor()
+            }
 
-          props.openNewMapModal().then(doWeCreateNewMap => {
-            if (doWeCreateNewMap) {
-              debug('Creating new map.')
-              editorState.current = EDITOR_STATE_NOT_STARTED
-              setEditor(null)
+            props.openNewMapModal().then(createNewMap => {
+              if (createNewMap) {
+                debug('Creating new map.')
+                editorState.current = constants.EDITOR_STATE.NOT_STARTED
+                setEditor(null)
+                return
+              }
+
+              // Unsuspend the editor.
+              if (editor instanceof Editor) {
+                editorState.current = constants.EDITOR_STATE.RUNNING
+                unsuspendEditor()
+              }
+            })
+          }}
+          saveMap={() => {
+            debug('Save map called')
+            if (!(editor instanceof Editor)) {
               return
             }
+            // Set the maximum amount of players in the map to the total of the
+            // max players in each team.
+            editor.mapConfig.maxPlayers = editor.mapConfig
+              .getTeams()
+              .map(team => team.maxPlayers)
+              .reduce((acc, current) => acc + current)
 
-            // Unsuspend the editor.
-            if (editor instanceof Editor) {
-              editorState.current = EDITOR_STATE_RUNNING
-              editor.unsuspend()
-            }
-          })
-        }}
-        saveMap={() => {
-          debug('Save map called')
-          if (!(editor instanceof Editor)) {
-            return
-          }
-          const exportedMapConf = new Blob([editor.mapConfig.exportAsJson()], {
-            type: 'application/json;charset=utf-8'
-          })
-          saveAs(exportedMapConf, `${editor.mapConfig.mapName}.json`)
-        }}
-      />
-      {/* We have to use state to do this because changing refs don't cause re-renders. */}
-      {editorLoading &&
-        <div
-          style={{
-            height: '100%',
-            width: '100%',
-            backgroundColor: 'rgb(172, 118, 61)',
-            zIndex: '101',
-            position: 'absolute',
-            top: '0',
-            bottom: '0'
+            const exportedMapConf = new Blob([editor.mapConfig.exportAsJson()], {
+              type: 'application/json;charset=utf-8'
+            })
+            saveAs(exportedMapConf, `${editor.mapConfig.mapName}.json`)
           }}
-        >
-          <Loading />
-        </div>}
-    </div>
+          openMapTeamsModal={() => {
+            // Suspend the editor while the user messes with the teams modal.
+            suspendEditor()
+            setTeamsModalOpen(true)
+          }}
+          openSettingsModal={() => {
+            suspendEditor()
+            setSettingsModalOpen(true)
+          }}
+        />
+        {/* We have to use state to do this because changing refs don't cause re-renders. */}
+        {editorLoading &&
+          <div
+            style={{
+              height: '100%',
+              width: '100%',
+              backgroundColor: 'rgb(172, 118, 61)',
+              zIndex: '101',
+              position: 'absolute',
+              top: '0',
+              bottom: '0'
+            }}
+          >
+            <Loading />
+          </div>}
+      </div>
+      <EditorModals
+        mapConfig={editor?.mapConfig}
+        forceUpdate={forceUpdate}
+        teamsModal={{
+          open: teamsModalOpen,
+          setOpen: setTeamsModalOpen
+        }}
+        mapSettingsModal={{
+          open: settingsModalOpen,
+          setOpen: setSettingsModalOpen
+        }}
+        unsuspendEditor={unsuspendEditor}
+        setError={props.setError}
+        vwDimensions={props.vwDimensions}
+      />
+    </>
   )
 }

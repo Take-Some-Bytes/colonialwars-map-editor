1e7d2bdac1febb9248fd1d5e69f4d02c05d8903e
Take-Some-Bytes <horton0712@gmail.com>
2022-08-03 12:48:07 -0700

    Migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview Vite configuration file.
+ */
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
